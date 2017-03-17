ce7085a96b17b5b29e2c90d3a8762b8ddadbdb72
hiro4869 <sakuragima48@gmail.com>
2017-03-17 20:51:19 +0900

    モデルbook.rb設定変更

--- bookapp/app/models/book.rb
+++ bookapp/app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
-  has_many :comments
-  has_many :quotations
+  has_many :comments, dependent: :destroy, inverse_of: :book
+  has_many :quotations, dependent: :destroy, inverse_of: :book
+
+  accepts_nested_attributes_for :comments, allow_destroy: true
+  accepts_nested_attributes_for :quotations, allow_destroy: true
 
   validates :title, presence: true
   validates :price, presence: true
