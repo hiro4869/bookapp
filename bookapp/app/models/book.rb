e3b8fa797b18657211c2d321bce0a7179f269a40
hiro4869 <sakuragima48@gmail.com>
2017-03-18 19:28:31 +0900

    cocoon完成

--- bookapp/app/models/book.rb
+++ bookapp/app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-  has_many :comments, inverse_of: :book
-  # has_many :quotations, dependent: :destroy, inverse_of: :book
+  has_many :comments, dependent: :destroy, inverse_of: :book
+  has_many :quotations, dependent: :destroy, inverse_of: :book
 
   accepts_nested_attributes_for :comments, reject_if: :all_blank, allow_destroy: true
-  # accepts_nested_attributes_for :quotations, allow_destroy: true
+  accepts_nested_attributes_for :quotations, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true
   validates :price, presence: true
